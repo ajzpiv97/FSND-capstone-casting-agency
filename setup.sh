4a92a9119fe45eb0f77e6fb2873eb14c516761c6
Armando Zubillaga <ajzpiv97@gmail.com>
2020-09-20 14:28:27 -0400

    Added my values for auth0 and db

---
+++ setup.sh
@@ -0,0 +1,8 @@
+export AUTH0_DOMAIN="dev-6jw0w2cu.us.auth0.com"
+export ALGORITHMS="RS256"
+export API_AUDIENCE="casting-agency"
+
+export DATABASE_URL="postgres://localhost:5432/casting"
+export FLASK_APP=flaskr
+export FLASK_DEBUG=True
+export FLASK_ENVIRONMENT=debug
