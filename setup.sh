2d4641a8bbdbbd9b2f8b80860e072b02c3a81578
Armando Zubillaga <ajzpiv97@gmail.com>
2020-09-21 23:28:38 -0400

    Renamed postgres

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ export AUTH0_DOMAIN="dev-6jw0w2cu.us.auth0.com"
 export ALGORITHMS="RS256"
 export API_AUDIENCE="casting-agency"
 
-export DATABASE_URL="postgres://localhost:5432/casting"
+export DATABASE_URL="postgresql://localhost:5432/casting"
 export FLASK_APP=flaskr
 export FLASK_DEBUG=True
 export FLASK_ENVIRONMENT=debug
